61e582ef6042f93c56d312ad220c0c55804e3d81
Diego Faria <diegofndefaria@gmail.com>
2016-07-29 13:43:36 -0300

    added dotenv

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 var mongoose = require('mongoose')
-var config = require('./config')
+var dotenv = require('dotenv')
 var setupController = require('./controllers/setupController')
 var apiController = require('./controllers/apiController')
 var Todo = require('./models/todoModel')
+
+dotenv.load({ path: '.env.example' });
 
 var port = process.env.PORT || 3000
 
@@ -22,7 +24,7 @@ app.get('/', function(req, res) {
     })
 })
 
-mongoose.connect(config.getDbConnectionString())
+mongoose.connect(process.env.MONGODB_URI)
 setupController(app)
 apiController(app)
 
