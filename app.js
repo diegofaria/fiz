f593dec13fc05cf214c7f803b7b90ccd9baf0759
Diego Faria <diegofndefaria@gmail.com>
2016-08-03 09:15:29 -0300

    implementing user authentication

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+var dotenv = require('dotenv')
+var mongoose = require('mongoose')
 var express = require('express')
 var app = express()
-var mongoose = require('mongoose')
-var dotenv = require('dotenv')
+
+var passport = require('passport')
+var session = require('express-session')
+var RedisStore = require('connect-redis')(session)
+
+var config = require('./config')
 var setupController = require('./controllers/setupController')
 var apiController = require('./controllers/apiController')
 var Todo = require('./models/todoModel')
+
 
 dotenv.load({ path: '.env.example' });
 
@@ -14,9 +21,23 @@ app.use('/assets', express.static(__dirname + '/public'))
 app.use('/vendor/bulma/', express.static(__dirname + '/node_modules/bulma/css'))
 app.use('/vendor/jquery/', express.static(__dirname + '/node_modules/jquery/dist'))
 
+app.use(session({
+    store: new RedisStore({
+        url: config.redisStore.url
+    }),
+    secret: config.redisStore.secret,
+    resave: false,
+    saveUninitialized: false
+}))
+
+
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
+    res.render('login')
+})
+
+app.get('/todos', function(req, res) {
     Todo.find({}, function(err, todos) {
         if (err) throw err
 
